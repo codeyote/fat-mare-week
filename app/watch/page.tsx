34ef5799b1b13852ada64c19a6eaa23814a379fb
Alex Smith <feral.codeyote@gmail.com>
2024-03-25 12:19:36 -0600

    add autoplay to video element

--- app/watch/page.tsx
+++ app/watch/page.tsx
@@ -4,7 +4,7 @@ import { sendTextAlert } from '../lib/actions';
 export default function Page() {
   return (
     <main className="flex h-dvh flex-col" >
-        <Video className="flex aspect-video h-96" src='https://test-streams.mux.dev/x36xhzz/url_6/193039199_mp4_h264_aac_hq_7.m3u8' />
+        <Video autoPlay className="flex aspect-video h-96" src='https://test-streams.mux.dev/x36xhzz/url_6/193039199_mp4_h264_aac_hq_7.m3u8' />
         <form action={sendTextAlert}>
             <button className="flex items-center gap-5 self-start rounded-lg bg-red-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-red-400 md:text-base">
                 Send Alert
